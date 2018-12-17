078406a4a913f155377b3d1d74abedb27d726197
Tyler Gerwig <eit_tcgerwig@Tylers-Mac-mini.local>
2018-12-17 10:08:59 -0600

    Changed results from a block to a controller for a page

---
+++ src/Controller/ResultsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\cse_selector\Controller;
+use Drupal\core\Controller\ControllerBase;
+
+/*
+ * Provides a search results response for module route to search-results
+ */
+
+class ResultsController extends ControllerBase {
+  /*
+   * @return array
+   *  Returns form for results
+   */
+  public function resultsPage() {
+    $form = \Drupal::formBuilder()->getForm('Drupal\cse_selector\Form\SearchForm');
+    return $form;
+  }
+}
