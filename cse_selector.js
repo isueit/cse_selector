881da6f3e6fd5f4a7b8b4828ab4d7a2e777f3626
Tyler Gerwig <eit_tcgerwig@eit-studweb-mac.exnet.iastate.edu>
2019-03-07 16:53:51 -0600

    Added switched param and removed op param from search bar

--- cse_selector.js
+++ cse_selector.js
@@ -8,3 +8,13 @@ function cse_selector_js_request() {
   s.parentNode.insertBefore(gcse, s);
 }
 //This function is also called when there is an ajax on_change event for the search wideness radios
+
+//Adds input element named switch that is hidden if user changes search_broadness with the radios
+function add_param() {
+  var searchUrl = document.location;
+  var node = document.createElement('INPUT');
+  node.type = 'hidden';
+  node.name = 'switched';
+  node.value = true;
+  document.getElementById('cse-selector-results-form').appendChild(node);
+}
